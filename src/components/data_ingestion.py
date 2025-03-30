d3f7aef22205d770b596377a6b54450b2f316029
Rakshitha Kolluru <rakshithakolluru7@gmail.com>
2025-03-30 17:01:34 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,9 +1,6 @@
 import os
 import sys
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
-import CustomException
+from src.exception import CustomException
 import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
